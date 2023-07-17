8ec139482b0db6a379d686896342877ad2624e0e
vivaanmittal <51257548+vivaan2002@users.noreply.github.com>
2023-07-18 01:43:52 +0530

    link

--- src/Components/Navbarcal.jsx
+++ src/Components/Navbarcal.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Navbarcal = () => {
   return (
     <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Image Reader</Navbar.Brand>
+        <Navbar.Brand to="/">Image Reader</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About Us</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
-            <Nav.Link href="/faq">FAQ</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
